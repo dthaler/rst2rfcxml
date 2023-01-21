cbcd017a01a978b8fe7745a328884909883ea8ef
Dave Thaler <dthaler@microsoft.com>
2023-01-20 16:49:08 -0800

    Initial empty program
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ rst2rfcxml/rst2rfcxml.cpp
@@ -0,0 +1,12 @@
+﻿// rst2rfcxml.cpp : Defines the entry point for the application.
+//
+
+#include "rst2rfcxml.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
