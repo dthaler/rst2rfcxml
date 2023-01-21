cc468f5da0235833a9450d7f2ba948e92548b797
Dave Thaler <dthaler@microsoft.com>
2023-01-21 10:31:24 -0800

    Handle multiple input files
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- rst2rfcxml/rst2rfcxml.h
+++ rst2rfcxml/rst2rfcxml.h
@@ -5,4 +5,26 @@
 
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+enum class rst_context {
+    NONE,
+    TITLE,
+};
+
+class rst2rfcxml {
+public:
+    void process_files(std::vector<std::string> input_filenames, std::ostream& output_stream);
+
+private:
+    void process_file(std::istream& input_stream, std::ostream& output_stream);
+    void process_line(std::string line, std::ostream& output_stream);
+    void output_header(std::ostream& output_stream) const;
+    void output_footer(std::ostream& output_stream) const;
+
+    std::string _document_name;
+    std::string _ipr;
+    std::string _category;
+    std::string _abbreviated_title;
+    rst_context _context;
+    int _section_depth = 0;
+    std::string _previous_line;
+};
