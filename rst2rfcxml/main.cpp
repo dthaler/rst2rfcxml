cca1b597fe98e0c53ce171e1455259d47f3a38f7
Dave Thaler <dthaler@microsoft.com>
2023-03-10 11:40:34 -0800

    Declare version 1.0.0
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- rst2rfcxml/main.cpp
+++ rst2rfcxml/main.cpp
@@ -4,26 +4,29 @@
 #include "CLI11.hpp"
 #include "rst2rfcxml.h"
 
+#define VERSION "rst2xmlrfc 1.0.0"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	CLI::App app{ "A reStructured Text to xml2rfc Version 3 converter" };
-	string output_filename;
-	app.add_option("-o", output_filename, "Output filename");
-	vector<string> input_filenames;
-	app.add_option("-i,input", input_filenames, "Input filenames")->mandatory(true);
-	CLI11_PARSE(app, argc, argv);
+    CLI::App app{ "A reStructured Text to xml2rfc Version 3 converter" };
+    app.set_version_flag("--version", std::string(VERSION));
+    string output_filename;
+    app.add_option("-o", output_filename, "Output filename");
+    vector<string> input_filenames;
+    app.add_option("-i,input", input_filenames, "Input filenames")->mandatory(true);
+    CLI11_PARSE(app, argc, argv);
 
-	rst2rfcxml rst2rfcxml;
-	if (output_filename.empty()) {
-		return rst2rfcxml.process_files(input_filenames, cout);
-	} else {
-		ofstream outfile(output_filename);
-		if (!outfile.good()) {
-			std::cerr << "ERROR: can't write " << output_filename << endl;
-			return 1;
-		}
-		return rst2rfcxml.process_files(input_filenames, outfile);
-	}
+    rst2rfcxml rst2rfcxml;
+    if (output_filename.empty()) {
+        return rst2rfcxml.process_files(input_filenames, cout);
+    } else {
+        ofstream outfile(output_filename);
+        if (!outfile.good()) {
+            std::cerr << "ERROR: can't write " << output_filename << endl;
+            return 1;
+        }
+        return rst2rfcxml.process_files(input_filenames, outfile);
+    }
 }
